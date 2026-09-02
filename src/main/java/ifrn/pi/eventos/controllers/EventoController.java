bdb4019202ac405887811e6f3c68a7061c4098f2
João Vitor de Melo Costa <20241204010031@ifrn.local>
2026-09-02 11:02:08 -0300

    Criando repository para Evento

--- src/main/java/ifrn/pi/eventos/controllers/EventoController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventoController.java
@@ -1,14 +1,31 @@
 package ifrn.pi.eventos.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import ifrn.pi.eventos.models.Evento;
+import ifrn.pi.eventos.repositories.EventoRepostirory;
 
 @Controller
 public class EventoController {
+	
+	@Autowired
+	private EventoRepostirory er;
 	
 	@RequestMapping("/eventos/form")
 	public String form() {
 		return "FormEvento";
 	}
+	
+	@PostMapping("/eventos")
+	public String adicionar(Evento evento) {
+	
+	System.out.println(evento);
+	er.save(evento);
+	
+	return "evento-adicionado";
 
+}
 }
